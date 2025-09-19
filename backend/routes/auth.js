e8ca73dfa3d6821c276bb08d484fc5ff8ca37302
Igor Dias <ihfdias.dev@gmail.com>
2025-09-19 17:42:01 +0000

    retirando o console.log do backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -24,10 +24,7 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email });
-
-    // ADICIONE ESTA LINHA PARA O NOSSO TESTE:
-    console.log('USUÁRIO ENCONTRADO NO BANCO DE DADOS:', user);
+    const user = await User.findOne({ email }); 
 
     if (!user) {
       return res.status(400).json({ msg: 'Credenciais inválidas.' });
