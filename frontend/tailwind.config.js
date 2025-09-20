c69c4f9dacbd86b043a9169c79259febe35a2cd5
Igor Dias <ihfdias.dev@gmail.com>
2025-09-20 12:59:09 +0000

    mudança da paleta de cores

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}", ],
   theme: {
     extend: {
       colors: {
-        'primary': '#003366',       
-        'secondary': '#f6993f',     
-        'success': '#38c172',      
-        'danger': '#e3342f',        
-        'accent': '#9561e2',        
-        'background': '#197bddff'   
+        'primary': '#2563eb',   
+        'success': '#16a34a',   
+        'danger': '#dc2626',    
+        'background': '#f8fafc' 
       },
     },
   },
