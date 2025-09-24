0f977019fc2478dda3f6f4347066474f37dab069
Igor Dias <ihfdias.dev@gmail.com>
2025-09-24 16:34:28 +0000

    configuração page inicial

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate, Link } from 'react-router-dom';
 import Footer from './Footer';
 
 function MainLayout() {
